0f63fcb817802a2de1e301ec41b6c1f071c983f3
melodreak <melodreak@yahoo.com>
2025-11-11 10:33:50 -0300

    removendo linhas desnecessarias

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
-import { GithubSecao } from './containers/Sobre/styles'
 import TemaLight from './themes/light'
 import TemaDark from './themes/dark'
 
@@ -20,9 +19,9 @@ function App() {
     <ThemeProvider theme={estaUsandoTemaDark ? TemaDark : TemaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar 
-          estaUsandoTemaDark={estaUsandoTemaDark} 
-          trocarTema={trocarTema} 
+        <Sidebar
+          estaUsandoTemaDark={estaUsandoTemaDark}
+          trocarTema={trocarTema}
         />
         <main>
           <Sobre />
